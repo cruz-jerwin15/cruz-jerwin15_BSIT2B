bd7026d7eb1b754759b63510d1031fb863451d98
Jerwin Cruz <cruz.jerwin15@gmail.com>
2025-09-18 07:49:37 +0800

    Adding server

---
+++ itb_server/db.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2')
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user:'root',
+    password:'',
+    database:'db_cruz',
+    waitForConnections:true,
+    connectionLimit:10,
+    queue:0
+})
+
+module.exports = pool.promise();
